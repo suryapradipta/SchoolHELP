115a066833f6ae885d44ed040574957493d121e3
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2022-11-22 02:08:16 +0800

    Update home-administrator.php

--- pages/home-administrator.php
+++ pages/home-administrator.php
@@ -202,11 +202,11 @@ if ($_SESSION['loginas'] != 'administrator') {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                Select "Logout" below if you are ready to.
+                Select "Confirm" to log out.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="logout.php">Logout</a>
+                <a class="btn btn-primary" href="logout.php">Confirm</a>
             </div>
         </div>
     </div>
